11c9a5f6a7b14d25e2f0bc0f6a46e9dca9c95229
muratpala007 <muratpala007@gmail.com>
2022-04-19 16:23:25 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _54DersForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            int[] sayilar = { 1, 2, 3, 4, 5 };
+            foreach(var item in sayilar)
+            {
+                toplam += item;
+            }
+            Console.WriteLine(toplam);
+        }
+    }
+}
